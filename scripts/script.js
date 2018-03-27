e8c0d0cad672c9581a96d09d602372d5fb2a9fa1
John Valle <dwanevalle.dv@gmail.com>
2018-03-27 23:02:31 +0800

    added about us

--- scripts/script.js
+++ scripts/script.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
                             <a class="nav-link" href="index.html" style="color: white">Home</a>
                         </li>
                         <li class="nav-item" style="padding: 0 15px;">
-                            <a class="nav-link" href="#" style="color: white">About Us</a>
+                            <a class="nav-link" href="about.html" style="color: white">About Us</a>
                         </li>
                         <li class="nav-item" style="padding: 0 15px;">
                             <a class="nav-link" href="faqs.html" style="color: white">FAQs</a>
@@ -70,8 +70,53 @@ $(document).ready(function(){
             </div>
         </div>
     </div>`;
+    var additionalInfo = `
+    <div class="container-fluid" style="background-color: #f2d2d3">
+    <div class="row" style="padding: 50px">
+        <div class="col-md-3">
+            <h6 style="color: #5b2e33" class="head-text">Dr. WebHealth</h6>
+            <ul class="body-text">
+                <small>
+                    <a id="footer-link" href="index.html"><li>Home</li></a>
+                    <a id="footer-link" href="about.html"><li>About Us</li></a>
+                    <a id="footer-link" href="faqs.html"><li>FAQs</li></a>
+                    <a id="footer-link" href="contact.html"><li>Contact Us</li></a>
+                </small>
+            </ul>
+        </div>
+        <div class="col-md-3">
+            <h6 style="color: #5b2e33" class="head-text">Health Issues</h6>
+            <ul class="body-text">
+                <small>
+                    <li>Cancer</li>
+                    <li>Diarrhea</li>
+                    <li>Parasites</li>
+                </small>
+            </ul>
+        </div>
+        <div class="col-md-3">
+            <h6 style="color: #5b2e33" class="head-text">Developers</h6>
+            <ul class="body-text">
+                <small>
+                    <li>De Leon, Justin Andre D.</li>
+                    <li>Lavilla, Christian C.</li>
+                    <li>Mejorada, Ara Marie C.</li>
+                    <li>Valle, John D.</li>
+                </small>
+            </ul>
+        </div>
+        <div class="col-md-3">
+            <h6 style="color: #5b2e33" class="head-text">Contact Information</h6>
+            <ul class="body-text">
+                <small>
+                    <li>Bo. Obrero, Iñigo St. Davao City, Philippines 8000</li>
+                </small>
+            </ul>
+        </div>
+    </div>`;
     $('.footnote').append(footnote);
     $('.head-tags').append(head_tags);
     $('.navigation').append(navigation);
     $('.banner').append(banner);
+    $('.additional-info').append(additionalInfo);
 });
